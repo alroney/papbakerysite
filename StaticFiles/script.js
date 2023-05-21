f148f58fe8c689653c8d085f70713c87f289fd5b
alroney <andrewroney@rocketmail.com>
2023-05-21 13:57:14 -0400

    Change Log:     - Complete redo of the website

--- StaticFiles/script.js
+++
@@ -1,9 +0,0 @@
-function mobileNavToggle() {
-    var x = document.getElementById("mobile-nav");
-    if (x.style.display === "block") { 
-        x.style.display = "none";
-    }
-    else {
-        x.style.display = "block";
-    }
-}
