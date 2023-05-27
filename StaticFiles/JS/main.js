61f232d8f9a2846fdac8c64ac26b34444e9eb891
alroney <andrewroney@rocketmail.com>
2023-05-26 20:12:17 -0400

    lots of change

--- StaticFiles/JS/main.js
+++ StaticFiles/JS/main.js
@@ -109,12 +109,14 @@
 
 })(jQuery);
 
-function initMap() {
-    // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
-    }
+
+//Function: To show and hide the dropdown menu
+$(function() {
+    $('.dropdown').on('show.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown()
+    });
+
+    $('.dropdown').on('hide.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp()
+    });
+})
