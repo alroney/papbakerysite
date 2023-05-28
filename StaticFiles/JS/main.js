eba4135b38e4722500c6bd4a4444f44430a87b18
alroney <andrewroney@rocketmail.com>
2023-05-27 20:22:49 -0400

    Changes:
    Added a new file: TemplateFiles/product.html
    updated TemplateFiles/index.html
    updated TemplateFiles/about.html
    updated TemplateFiles/MoreAbout/papstory.html
    updated StaticFiles/JS/main.js
    updated StaticFiles/CSS/owl.carousel.min.css
    updated StaticFiles/CSS/main-style.css
    updated StaticFiles/CSS/PageCSS/home-style.css

--- StaticFiles/JS/main.js
+++ StaticFiles/JS/main.js
@@ -34,10 +34,10 @@
                 items:1
             },
             600:{
-                items:1
+                items:2
             },
             1000:{
-                items:1
+                items:3
             }
         }
     })
@@ -65,6 +65,7 @@
         }
     })
 
+    //Keeps the navbar fixed on top when scrolling
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
              $('.navbar').addClass('active');
