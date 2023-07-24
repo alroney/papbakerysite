7696387716d3a6ef264622885a7540dbb2804ee2
alroney <andrewroney@rocketmail.com>
2023-07-24 10:24:59 -0400

    order form attempt

---
+++ StaticFiles/JS/PageJS/product.js
@@ -0,0 +1,34 @@
+/* ORDER FORM */
+var form = document.getElementById("orderForm");
+async function handleSubmit(event) {
+    event .preventDefault();
+    var status = document.getElementById("status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if(response.ok) {
+            status.innerHTML = "Thanks for your order! We'll get back to you soon.";
+            form.reset()
+        } 
+        else {
+            response.json().then(data => {
+                if(Object.hasOwn(data, "error")) {
+                    status.innerHTML = data.error.map(error => error["message"]).join(", ");
+                }
+                else {
+                    status.innerHTML = "Oops! There was a problem submitting your order"
+                }
+            })
+        }
+    }).catch(error => {
+        status.classList.add('error');
+        status.innerHTML = "Oops! There was a problem submitting your order"
+    });
+}
+form.addEventListener("submit", handleSubmit)
+/* END OF ORDER FORM */
