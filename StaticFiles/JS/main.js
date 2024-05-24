a9089e7c936958b4f5b164b314b6b1e7d29dc28b
alroney <andrewroney@rocketmail.com>
2024-05-24 09:31:41 -0400

    Small style adjustments

--- StaticFiles/JS/main.js
+++ StaticFiles/JS/main.js
@@ -211,19 +211,7 @@ function initNavbarScripts() {
         $('.navbar-collapse').removeClass('show');
     });
 
-    //Back to top button functionality
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-
-    $('.back-to-top').click(function() {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    
 }
 
 
